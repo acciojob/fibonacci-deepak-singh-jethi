2aef70f48e6f3ebc74f7ae24a95e7db189646ce1
DEEPAK SINGH JETHI <134581381+deepak-singh-jethi@users.noreply.github.com>
2023-11-13 19:37:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
-// your code here
+let a = 0;
+let b = 1;
+let temp = 0;
+for(let i = 1; i<num; i++){
+	temp = a; 
+	a = a+b;
+	b = temp;
+	
+}
+	return a; 
+	
+	
 }
 
 module.exports = fibonacci;
